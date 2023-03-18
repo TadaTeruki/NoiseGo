6136552e7e7a2e5a91b923b4cc757e1fd03a850c
Teruki Tada <tadateruki.public@gmail.com>
2023-03-18 17:45:53 +0900

    removed seed configuration

--- example.go
+++ example.go
@@ -1,16 +1,17 @@
 package main
 
-import(
+import (
 	"image"
-	"image/png"
 	"image/color"
+	"image/png"
 	"os"
-	noise "github.com/TadaTeruki/NoiseGo/Noise"
+
+	"github.com/TadaTeruki/NoiseGo/v2/noise"
 )
 
-func main(){
+func main() {
 
-	nz := noise.New(100, 30)
+	nz := noise.New(30)
 
 	width := 800
 	height := 800
@@ -19,17 +20,17 @@ func main(){
 	for iy := 0; iy < height; iy++ {
 		for ix := 0; ix < width; ix++ {
 
-			query_x := float64(ix)/float64(width)  * 30
-			query_y := float64(iy)/float64(height) * 30
+			query_x := float64(ix) / float64(width) * 30
+			query_y := float64(iy) / float64(height) * 30
 
 			noise := nz.Get(query_x, query_y)
 
-			color := color.RGBA{uint8(noise*0xff), uint8(noise*0xff), uint8(noise*0xff), 0xff}
+			color := color.RGBA{uint8(noise * 0xff), uint8(noise * 0xff), uint8(noise * 0xff), 0xff}
 			img.Set(ix, iy, color)
 		}
 	}
 
 	file, _ := os.Create("image.png")
 	png.Encode(file, img)
-	
+
 }
